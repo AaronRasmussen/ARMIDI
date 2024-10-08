cece0c38fa0a9597c04f7abed63afef04b8b7736
AaronRasmussen <50007456+AaronRasmussen@users.noreply.github.com>
2024-10-07 22:44:02 -0700

    added dx21 parser

---
+++ ARMIDIDX21/Sources/ARMIDIDX21SysExParser.swift
@@ -0,0 +1,16 @@
+//
+//  ARMIDIDX21SysExParser.swift
+//  ARMIDI
+//
+//  Created by Aaron Rasmussen on 10/7/24.
+//
+
+public class ARMIDIDX21SysExParser: ARMIDIParser {
+    
+    
+    public override func process(midiMessage m: ARMIDIParserMessage?) {
+        switch m {
+            
+        }
+    }
+}
--- ARMIDIParser/Sources/ARMIDIParser.swift
+++ ARMIDIParser/Sources/ARMIDIParser.swift
@@ -17,8 +17,8 @@ public class ARMIDIParser: ARMIDIParserType {
         self.currentState = state
     }
     
+    // Override this function
     public func process(midiMessage: ARMIDIParserMessage?) {
-        if let m = midiMessage { print(m) }
+        return
     }
-    
 }
