d6ac1feffe141f566051b00d8da3439d36a99a3f
AaronRasmussen <50007456+AaronRasmussen@users.noreply.github.com>
2024-10-14 20:46:34 -0700

    Create ARMIDIDX21+MIDIManager.swift

---
+++ ARMIDIDX21/Sources/ARMIDIDX21+MIDIManager.swift
@@ -0,0 +1,13 @@
+//
+//  ARMIDIDX21+MIDIManager.swift
+//  Pods
+//
+//  Created by Aaron Rasmussen on 10/14/24.
+//
+
+import Foundation
+
+public class DX21MIDIManager {
+    
+    let parser = ARMIDIDX21SysExParser()
+}
