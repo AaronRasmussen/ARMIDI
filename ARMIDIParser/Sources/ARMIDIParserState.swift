a1785b99f80767e5b47d1480cd6126bbbd566aea
AaronRasmussen <50007456+AaronRasmussen@users.noreply.github.com>
2024-10-17 23:14:13 -0700

    Update ARMIDIParserState.swift
    implemented CustomStringConvertible on ARMIDIParserState

--- ARMIDIParser/Sources/ARMIDIParserState.swift
+++ ARMIDIParser/Sources/ARMIDIParserState.swift
@@ -5,7 +5,7 @@
 //  Created by Aaron Rasmussen on 10/5/24.
 //
 
-public enum ARMIDIParserState {
+public enum ARMIDIParserState: CustomStringConvertible {
     
     case parsing(bytes: Data, index: Int)
     case parsingData(bytes: Data, index: Int, statusByte: UInt8, data: Data, expectedDataCount: Int, currentDataCount: Int)
@@ -32,6 +32,7 @@ public enum ARMIDIParserState {
     }
     
     public func addBytes(_ bytes: [UInt8]) -> ARMIDIParserState {
+        
         switch self {
             
         case .parsing(var bs, let i):
@@ -53,6 +54,27 @@ public enum ARMIDIParserState {
         case .running(var bs, let i, let sB):
             bs.append(contentsOf: bytes)
             return .running(bytes: bs, index: i, statusByte: sB)
+        }
+    }
+    
+    public var description: String {
+        
+        switch self {
+            
+        case .parsing(let bs, let i):
+            return "State: .parsing (\(bs.count) bytes, index \(i)) \nBytes: \(bs)"
+            
+        case .parsingData(let bs, let i, let sB, let d, let eBC, let cBC):
+            return "State: .parsingData (\(bs.count) bytes, index \(i); status byte: \(sB); expected byte count \(eBC); current byte count \(cBC)) \nBytes: \(bs)"
+            
+        case .parsingSystemExclusiveMessage(let bs, let i, let d):
+            return "State: .parsingSystemExclusiveMessage (\(bs.count) bytes, index \(i); data count: \(d.count); data: \(d)) \nBytes: \(bs)"
+            
+        case .parsingDataTail(let bs, let i, let d):
+            return "State: .parsingDataTail (\(bs.count) bytes, index \(i); data count: \(d.count); data: \(d)) \nBytes: \(bs)"
+            
+        case .running(let bs, let i, statusByte: let sB):
+            return "State: .running (\(bs.count) bytes, index \(i); status byte: \(sB) \nBytes: \(bs)"
         }
     }
 }
