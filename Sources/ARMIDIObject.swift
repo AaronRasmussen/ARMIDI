c2ccab4467c301cb7bd89a7787bb5e711aa8fc6e
AaronRasmussen <50007456+AaronRasmussen@users.noreply.github.com>
2025-02-04 00:59:25 -0800

    Added `ARMIDIProtocols.swift`
    Defined the `MIDIReferable` protocol.
    
    Began defining `MIDIObjectReferable`, implementing `getStringProperty(_:)`.

--- Sources/ARMIDIObject.swift
+++ Sources/ARMIDIObject.swift
@@ -32,3 +32,32 @@ public func findObject(withUniqueID uniqueID: MIDIUniqueID) throws -> (object: M
     
     return (object, type)
 }
+
+extension MIDIObjectReferable {
+    
+    /// Retrieves the identified `String` property.
+    ///
+    /// - Parameter property: A constant defined by `CoreMIDI` for referring to MIDI object properties.
+    ///
+    /// - Throws: `MIDIError`
+    ///
+    /// - Returns: A `String` value, or `nil` if the property is not set for the object.
+    public func getStringProperty(_ property: CFString) throws -> String? {
+        
+        var stringProperty: Unmanaged<CFString>? = nil
+        
+        let status = MIDIObjectGetStringProperty(self.midiRef, property, &stringProperty)
+        
+        guard
+            status != kMIDIUnknownProperty
+        else { return nil }
+        
+        guard
+            status == 0
+        else {
+            throw MIDIError(status)
+        }
+        
+        return stringProperty?.takeUnretainedValue() as String?
+    }
+}
