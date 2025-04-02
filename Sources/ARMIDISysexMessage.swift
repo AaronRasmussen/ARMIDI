d3e929b41c410416dfd54f8e9650f255fb19756b
AaronRasmussen <50007456+AaronRasmussen@users.noreply.github.com>
2025-04-01 22:32:55 -0700

    Added ARMIDISysex files

---
+++ Sources/ARMIDISysexMessage.swift
@@ -0,0 +1,78 @@
+//
+//  ARMIDISysexMessage.swift
+//  ARMIDISysex
+//
+//  Created by Aaron Rasmussen on 2/25/25.
+//
+
+import CoreMIDI
+import ARMIDIError
+import ARMIDI
+
+
+/// A system exclusive MIDI message.
+///
+/// `SysexMessage` is used to send a MIDI system exclusive message to a MIDI destination.
+///
+/// An empty `SysexMessage` is created using `SysexMessage()`. The data to send, the destination to send it to, and an optional completion handler are suppled wihen calling the `send(sysexData:to:completionhandler:)` instance method.
+public class SysexMessage {
+    
+    fileprivate var data: [UInt8] = []
+    fileprivate var handle: ((MIDISysexSendRequest) -> Void)? = nil
+    fileprivate var sendRequest: MIDISysexSendRequest! = nil
+    
+    /// Initialize a `SysexMessage`.
+    public init() { }
+    
+    /// Send a `SysexMessage` to a MIDI destination with an optional completion handler.
+    ///
+    /// - Parameters:
+    ///   - sysexData: The data to send. The array should begin with the MIDI system exclusive status byte `0xF0` and end with the `EOX` status byte `0xF7`.
+    ///   - destination: The MIDI destination that will receive the message.
+    ///   - completionHandler: A completion handler that will be called when the transmission is complete.
+    /// - Throws: `MIDIError`
+    public func send(_ data: [UInt8], to destination: DestinationReferable, completionHandler handler: ((MIDISysexSendRequest) -> Void)?) throws {
+        
+        let count = data.count
+
+        self.data = data
+        self.handle = handler
+        self.sendRequest = withUnsafePointer(to: &self.data[0]) { bytes in
+            MIDISysexSendRequest(
+                destination: destination.midiRef,
+                data: bytes,
+                bytesToSend: UInt32(count),
+                complete: false,
+                reserved: (0,0,0),
+                completionProc: sendRequestCompletionWrapper,
+                completionRefCon: Unmanaged.passRetained(self).toOpaque()
+            )
+        }
+        
+        let status = MIDISendSysex(&self.sendRequest)
+        
+        guard
+            status == 0
+        else {
+            throw MIDIError(status)
+        }
+    }
+}
+
+fileprivate func sendRequestCompletionWrapper(_ sysexSendRequestPointer: UnsafeMutablePointer<MIDISysexSendRequest>) -> Void {
+
+    let sysexSendRequest = sysexSendRequestPointer.pointee
+
+    guard
+        let unwrappedRawHandler = sysexSendRequest.completionRefCon
+    else {
+        return
+    }
+
+    let unmanagedHandler = Unmanaged<SysexMessage>.fromOpaque(unwrappedRawHandler)
+    let handler = unmanagedHandler.takeRetainedValue()
+
+    handler.handle?(sysexSendRequest)
+    handler.sendRequest = nil
+    handler.handle = nil
+}
---
+++ Sources/ARMIDISysexParser.swift
@@ -0,0 +1,74 @@
+//
+//  SysexParser.swift
+//  ARMIDISysex
+//
+//  Created by Aaron Rasmussen on 2/25/25.
+//
+
+import CoreMIDI
+import ARMIDI
+import ARMIDIError
+
+/// A parser for system exclusive MIDI messages.
+///
+/// The parser looks only for system exclusive MIDI messages. Messages will be handled by the `sysexHandler` supplied when the parser is initialized.
+public class SysexParser {
+    
+    private enum State {
+        case waiting
+        case receiving
+    }
+    
+    private let handle: ([UInt8]) -> Void
+    
+    private var state: State = .waiting
+    private var buffer: [UInt8] = []
+    
+    /// Initialize a parser.
+    ///
+    /// - Parameter sysexHandler: A handler for processing the system exclusive MIDI message.
+    public init(sysexHandler: @escaping ([UInt8]) -> Void) {
+        self.handle = sysexHandler
+    }
+    
+    /// A method for handling data to be parsed.
+    ///
+    /// - Parameter data: An array of bytes to be parsed.`
+    public func parse(_ data: [UInt8]) {
+        
+        data.forEach { byte in
+            
+            switch self.state {
+                
+            case .waiting:
+                
+                if byte == kSystemExclusive {
+                    self.state = .receiving
+                }
+                
+            case .receiving:
+                
+                switch byte {
+                    
+                case kSystemExclusive:
+                    self.buffer = []
+                    
+                case kSystemCommonEOX:
+                    self.handle(buffer)
+                    self.state = .waiting
+                    self.buffer = []
+                    
+                case let b where !b.isStatusByte:
+                    self.buffer.append(b)
+                    
+                case let b where b.isSystemRealTimeStatusByte:
+                    break
+                    
+                default:
+                    self.state = .waiting
+                    self.buffer = []
+                }
+            }
+        }
+    }
+}
