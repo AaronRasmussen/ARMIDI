7c90c43f262e38183913633e87bbe4dcee64698d
AaronRasmussen <50007456+AaronRasmussen@users.noreply.github.com>
2025-05-17 09:48:17 -0700

    Documented `ARMIDIBool`

--- Sources/ARMIDIBool.swift
+++ Sources/ARMIDIBool.swift
@@ -7,8 +7,15 @@
 
 extension Bool {
     
+    /// This is a computed property that returns `1` for `true` or `0` for `false`.
+    ///
+    /// It is used for compatibility with boolean properties that are stored by a synthesizer using a `UInt8` value.
     public var uint8: UInt8 { self ? 1 : 0 }
     
+    
+    /// Initialize a `Bool` value using a `UInt8`  value.
+    ///
+    /// A `0` returns  `false`. All other values return `true`.
     public init(_ value: UInt8) {
         self = value != 0
     }
